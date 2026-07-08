d34b989f5068417562c0e2af5db549d64f69483d
kl-demi <demi@klconvergence.com>
2026-07-08 11:13:33 -0400

    fix. change local port numbers to match container's

--- src/arxiv_graphdb/config.py
+++ src/arxiv_graphdb/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    neo4j_uri: str = "bolt://localhost:8008"
+    neo4j_uri: str = "bolt://localhost:7687"
     neo4j_user: str = "neo4j"
     neo4j_password: str = "changeme123"
 
